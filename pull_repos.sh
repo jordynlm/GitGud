b72377fc0feda4a12c4ccd9954bc5fc1e1813eca
Eyob Woldeghebriel <eyobw@mit.edu>
2019-09-14 13:47:05 -0400

    Updating pull_repos to recursively search in the found directories for more directories. It prints what directories it finds.

--- pull_repos.sh
+++ pull_repos.sh
@@ -1,7 +1,28 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-for f in *; do
-    if [[ -d ${f} && ! -L ${f} ]]; then
-        echo $f
-    fi
-done
+function print_all_dirs {
+    TABS=""
+    for ((i = 0; i < $2; i++))
+    do
+        TABS="$TABS\t"
+    done
+
+    for f in $1/*; do
+        if [[ -d ${f} && ! -L ${f} ]]; then
+            printf "$TABS"
+            printf "%s\n" "$f"
+            print_all_dirs $f $2+1
+        fi
+    done
+}
+
+function print_tabs {
+    for ((i = 0; i < $1; i++))
+    do
+        printf "\t"
+    done
+    printf "here\n"
+}
+
+print_all_dirs . 0
+
