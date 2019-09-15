a209a3a13e8dfa96a63ece6dcf42150b0b947fe4
tj-murphy <tjmurphy@mit.edu>
2019-09-14 20:35:46 -0400

    First pass at SO searcher integration

--- gitgudstackoverflow.py
+++ gitgudstackoverflow.py
@@ -2,19 +2,20 @@ import requests
 import re
 import sys
 
-query=""
-for line in sys.stdin:
-    query+=line
-    
-print("QUERY IS " + query)
+print(sys.argv[1:])
+query=" ".join(sys.argv[1:])
+print("Searching Stack Overflow for error message: " + query)
 queryplus = query.replace(" ","+")
-a = requests.get("https://stackoverflow.com/search?q="+queryplus)
+url = "https://stackoverflow.com/search?q="+queryplus
+print("Search URL "+ url)
+a = requests.get(url)
 resultspage = a.text
 result_ids = re.findall("(?<=question\-summary\-)\d*",resultspage)
+if len(result_ids) ==0:
+	print("No results found on Stack Overflow.")
+	sys.exit()
 topresulturl = "https://stackoverflow.com/questions/" + result_ids[0]
 topresult = requests.get(topresulturl).text
 topresultanswers = re.findall("(?<=<div class=\"post-text\" itemprop=\"text\">)[\s\S]*?(?=</div>)",topresult)
 print(topresultanswers[1])
-print('done')
-
 
