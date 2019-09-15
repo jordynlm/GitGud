f070f6d6ee967260ae2f2176ff5fe4d635f148df
Eyob Woldeghebriel <eyobw@mit.edu>
2019-09-15 09:06:48 -0400

    Adding stuff to setup.sh.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 # Prompt for and set GITGUD_PATH
+printf "Enter GitGud path: "
+read gitgud_path
+echo "export GITGUD_PATH=$gitgud_path" >> ~/.bashrc
+source ~/.bashrc
 
 # Add gitgud symlink
+sudo ln -s $GITGUD_PATH/gitgud /usr/bin/gitgud
 
 # Clone pizzapi
+git clone https://github.com/gamagori/pizzapi.git
+cd pizzapi
+sudo python3 setup.py install
+cd ..
 
 # Clone PyGithub
