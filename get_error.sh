65c7ad0785bd09be32101d04c9b649fdd0e7155b
Jordyn Mann <jordynm@mit.edu>
2019-09-14 21:08:41 -0400

    Modify error output to work with error output file

---
+++ get_error.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function get_error()
+{
+	error=$(eval $1 2>>gitgud_error_output.txt)
+	echo $error
+	python3 gitgudstackoverflow.py gitgud_error_output.txt
+	rm gitgud_error_output.txt
+}
+
+get_error $1
